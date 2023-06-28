d5384fa02573d82ec325abf365a4e482c9855260
Anthony Khaoya <112507926+antonixe@users.noreply.github.com>
2023-06-28 00:45:22 -0700

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-def pascal_triangle(n):
-    if n <= 0:
-        return []
-    result = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(result[i-1][j-1] + result[i-1][j])
-        row.append(1)
-        result.append(row)
-    return result
+'''A module for working with Pascal's triangle.
+'''
 
+
+def pascal_triangle(n):
+    '''Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
