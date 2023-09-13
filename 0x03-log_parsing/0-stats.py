f0814480067dd086e27629d46b553a8a2ded2a0a
Anthony Khaoya <112507926+antonixe@users.noreply.github.com>
2023-09-13 13:26:35 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,45 +4,56 @@
 """
 import sys
 
+import sys
+import re
+import signal
 
-if __name__ == '__main__':
-    # initialize variables to store metrics
-    total_file_size = 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    processed_line = 0
-    statistics = {}
-    for code in codes:
-        statistics[code] = 0
+# Global variables to hold stats
+total_size = 0
+status_code_count = {
+    200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0
+}
 
-    def print_statistics(statistics: dict, total_file_size: int):
-        print('File size: {}'.format(total_file_size))
-        for code, count in sorted(statistics.items()):
-            if count:
-                print("{}: {}".format(code, count))
+def print_stats(signum=None, frame=None):
+    """Function to print statistics"""
+    global total_size, status_code_count
+
+    print(f"Total file size: File size: {total_size}")
+    for code, count in sorted(status_code_count.items()):
+        if count > 0:
+            print(f"{code}: {count}")
+
+def main():
+    global total_size, status_code_count
+
+    # Register a signal handler for keyboard interruption
+    signal.signal(signal.SIGINT, print_stats)
+
+    # Regex pattern for parsing logs
+    pattern = r'(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[.*\] "GET /projects/260 HTTP/1.1" (?P<status>\d{3}) (?P<size>\d+)'
+
+    line_count = 0
 
     try:
         for line in sys.stdin:
-            processed_line += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in statistics:
-                    statistics[status_code] += 1
-            except BaseException:
-                # if status code isn't in the correct position, skip line
-                pass
-            try:
-                file_size = int(data[-1])
-                total_file_size += file_size
-            except BaseException:
-                # if file size isn't in the correct position, skip line
-                pass
+            match = re.search(pattern, line)
+            if match:
+                size = int(match.group("size"))
+                status = int(match.group("status"))
 
-            # After every 10 lines print statistics
-            if processed_line % 10 == 0:
-                print_statistics(statistics, total_file_size)
-        print_statistics(statistics, total_file_size)
+                total_size += size
+                if status in status_code_count:
+                    status_code_count[status] += 1
+
+                line_count += 1
+                if line_count % 10 == 0:
+                    print_stats()
+                    print("-" * 40)
     except KeyboardInterrupt:
-        # if the keyboard is interrupted (control C) print final statistics
-        print_statistics(statistics, total_file_size)
-        raise
+        pass
+
+    # Print final stats
+    print_stats()
+
+if __name__ == '__main__':
+    main()
